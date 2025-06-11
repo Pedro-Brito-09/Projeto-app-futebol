f538bff15edcfc2356e0ac80e713a754bdb191c2
Pedro-Brito-09 <pe.brito09@gmail.com>
2025-06-11 20:58:02 -0300

    Reupando minha versão do app
    Estou tirando o Kairos pois ele espera até as ultimas horas para começar a fazer a parte dele (ctrl+c, ctrl+v do chat gpt), começa a dar commit faltando 5 minutos para o prazo (nem testa se funciona), as adições dele quebraram o projeto todo (avisei ele sobre o prazo 4 dias antes)

---
+++ ProjetoOOPApp/ProjetoOOP/Enums/ModosDePartida.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoOOP.Enums;
+
+public enum ModosDePartida
+{
+    GANHA_FICA = 1,
+    DOIS_JOGOS = 2,
+}
